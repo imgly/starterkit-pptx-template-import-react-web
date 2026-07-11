c31f13b0f6479b51b1007a57e61cd50770facb66
Intergalactic Igor <malte.baumann+bot@img.ly>
2026-07-11 11:04:24 +0200

    Update starterkit for v1.78.0-rc.1

--- src/imgly/index.ts
+++ src/imgly/index.ts
@@ -5,7 +5,7 @@
  * import editor. Import and call `initPptxTemplateImportEditor()` to configure
  * a CE.SDK instance for editing imported PowerPoint (PPTX) templates.
  *
- * @see https://img.ly/docs/cesdk/js/features/
+ * @see https://img.ly/docs/cesdk/js/key-capabilities-dbb5b1/
  */
 
 import type CreativeEditorSDK from '@cesdk/cesdk-js';
