a53ead7087ebfe3f0680f05f116a7f882f295336
Intergalactic Igor <malte.baumann+bot@img.ly>
2026-09-18 20:15:32 +0200

    Update starterkit for v1.82.1-rc.1

--- src/imgly/plugins/pptx-importer.ts
+++ src/imgly/plugins/pptx-importer.ts
@@ -20,14 +20,14 @@
  * });
  *
  * // Load into editor
- * await cesdk.loadFromArchiveURL(result.sceneArchiveUrl);
+ * await cesdk.load(result.sceneArchiveUrl);
  *
  * // Clean up when done (at app level)
  * URL.revokeObjectURL(result.imageUrl);
  * URL.revokeObjectURL(result.sceneArchiveUrl);
  * ```
  *
- * @see https://img.ly/docs/cesdk/js/features/import-pptx/
+ * @see https://img.ly/docs/cesdk/js/starterkits/pptx-template-import-pptx01/
  */
 
 import CreativeEngine from '@cesdk/engine';
